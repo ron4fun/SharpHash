6c6c468ac042aef7f4f1734fa49c5978d88e7e79
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2019-10-14 14:02:45 +0100

    Added tests for Hash128 hashes

---
+++ SharpHash.Tests/Hash128/MurmurHash3_x64_128Tests.cs
@@ -0,0 +1,133 @@
+using SharpHash.Hash128;
+using SharpHash.Interfaces;
+using SharpHash.Utils;
+using SharpHash.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace SharpHash.Hash128.Tests
+{
+    [TestClass]
+    public class MurmurHash3_x64_128Tests
+    {
+        protected IHash hash = new MurmurHash3_x64_128();
+
+        protected string ExpectedHashOfEmptyData = "00000000000000000000000000000000";
+        protected string ExpectedHashOfDefaultData = "705BD3C954B94BE056F06B68662E6364";
+        protected string ExpectedHashOfRandomString = "D30654ABBD8227E367D73523F0079673";
+        protected string ExpectedHashOfZerotoFour = "0F04E459497F3FC1ECCC6223A28DD613";
+        protected string ExpectedHashOfEmptyDataWithOneAsKey = "4610ABE56EFF5CB551622DAA78F83583";
+        protected string ExpectedHashOfDefaultDataWithMaxUInt32AsKey = "ADFD14988FB1F8582A1B67C1BBACC218";
+
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            TestHelper.TestActualAndExpectedData(TestConstants.EmptyData,
+                ExpectedHashOfEmptyData, hash);
+        }
+
+        [TestMethod]
+        public void TestDefaultData()
+        {
+            TestHelper.TestActualAndExpectedData(TestConstants.DefaultData,
+                ExpectedHashOfDefaultData, hash);
+        }
+
+        [TestMethod]
+        public void TestRandomString()
+        {
+            TestHelper.TestActualAndExpectedData(TestConstants.RandomStringTobacco,
+                ExpectedHashOfRandomString, hash);
+        }
+
+        [TestMethod]
+        public void TestZerotoFour()
+        {
+            TestHelper.TestActualAndExpectedData(TestConstants.ZerotoFour,
+                ExpectedHashOfZerotoFour, hash);
+        }
+
+        [TestMethod]
+        public void TestEmptyStream()
+        {
+            TestHelper.TestEmptyStream(ExpectedHashOfEmptyData, hash);
+        }
+
+        [TestMethod]
+        public void TestIncrementalHash()
+        {
+            TestHelper.TestIncrementalHash(TestConstants.DefaultData,
+                ExpectedHashOfDefaultData, hash);
+        }
+
+        [TestMethod]
+        public void TestIndexChunkedDataIncrementalHash()
+        {
+            Int32 Count, i;
+            byte[] temp, ChunkedDataBytes;
+            string ActualString, ExpectedString;
+
+            ChunkedDataBytes = Converters.ConvertStringToBytes(TestConstants.ChunkedData);
+            for (i = 0; i < ChunkedDataBytes.Length; i++)
+            {
+                Count = ChunkedDataBytes.Length - i;
+
+                temp = new byte[Count];
+                Utils.Utils.memcopy(temp, ChunkedDataBytes, Count, i);
+
+                hash.Initialize();
+
+                hash.TransformBytes(ChunkedDataBytes, i, Count);
+
+                ActualString = hash.TransformFinal().ToString();
+                ExpectedString = new MurmurHash3_x64_128().ComputeBytes(temp).ToString();
+
+                Assert.AreEqual(ExpectedString, ActualString);
+            }
+        }
+
+        [TestMethod]
+        public void TestHashCloneIsCorrect()
+        {
+            TestHelper.TestHashCloneIsCorrect(hash);
+        }
+
+        [TestMethod]
+        public void TestHashCloneIsUnique()
+        {
+            TestHelper.TestHashCloneIsUnique(hash);
+        }
+
+        [TestMethod]
+        public void TestWithDifferentKeyOneEmptyString()
+        {
+            IHashWithKey LIHashWithKey;
+
+            string ExpectedString = ExpectedHashOfEmptyDataWithOneAsKey;
+            LIHashWithKey = (hash as IHashWithKey);
+            LIHashWithKey.Key = Converters.ReadUInt32AsBytesLE((UInt32)1);
+
+            string ActualString = LIHashWithKey.ComputeString(TestConstants.EmptyData).ToString();
+
+            Assert.AreEqual(ExpectedString, ActualString);
+        }
+
+        [TestMethod]
+        public void TestWithDifferentKeyMaxUInt32DefaultData()
+        {
+            IHashWithKey LIHashWithKey;
+
+            string ExpectedString = ExpectedHashOfDefaultDataWithMaxUInt32AsKey;
+            LIHashWithKey = (hash as IHashWithKey);
+            LIHashWithKey.Key = Converters.ReadUInt32AsBytesLE(UInt32.MaxValue);
+
+            string ActualString = LIHashWithKey.ComputeString(TestConstants.DefaultData).ToString();
+
+            Assert.AreEqual(ExpectedString, ActualString);
+        }
+
+    }
+
+}
