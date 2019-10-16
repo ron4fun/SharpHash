2e81d61a1814aebeb3cf680eeb2f6c536d6b0fa2
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2019-10-16 06:50:08 +0100

    Changed enum name and added more hash sizes

--- SharpHash/Base/HashSize.cs
+++ SharpHash/Base/HashSize.cs
@@ -1,13 +1,16 @@
 ﻿
 namespace SharpHash.Base
 {
-    public enum HashSize
+    // Note: The name HashSizeEnum was given considering the name conflict
+    // between the enum and the Hash property for getting hash_size.
+    public enum HashSizeEnum
     {
         HashSize128 = 16,
         HashSize160 = 20,
         HashSize192 = 24,
         HashSize224 = 28,
         HashSize256 = 32,
+        HashSize288 = 36,
         HashSize384 = 48,
         HashSize512 = 64
     }
