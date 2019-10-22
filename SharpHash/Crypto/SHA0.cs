abcc8eb13c0f9d705116f545e1a72ef0ede7bfa4
Mbadiwe Nnaemeka Ronald <ron2tele@gmail.com>
2019-10-22 04:34:49 +0100

    Updated license info

--- SharpHash/Crypto/SHA0.cs
+++ SharpHash/Crypto/SHA0.cs
@@ -1,3 +1,28 @@
+///////////////////////////////////////////////////////////////////////
+/// SharpHash Library
+/// Copyright(c) 2019  Mbadiwe Nnaemeka Ronald
+/// Github Repository <https://github.com/ron4fun/SharpHash>
+///
+/// The contents of this file are subject to the
+/// Mozilla Public License Version 2.0 (the "License");
+/// you may not use this file except in
+/// compliance with the License. You may obtain a copy of the License
+/// at https://www.mozilla.org/en-US/MPL/2.0/
+///
+/// Software distributed under the License is distributed on an "AS IS"
+/// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+/// the License for the specific language governing rights and
+/// limitations under the License.
+///
+/// Acknowledgements:
+///
+/// Thanks to Ugochukwu Mmaduekwe (https://github.com/Xor-el) for his creative
+/// development of this library in Pascal/Delphi (https://github.com/Xor-el/HashLib4Pascal).
+///
+/// Also, I will like to thank Udezue Chukwunwike (https://github.com/IzarchTech) for
+/// his contributions to the growth and development of this library.
+///
+////////////////////////////////////////////////////////////////////////
 using SharpHash.Base;
 using SharpHash.Interfaces;
 using SharpHash.Utils;
